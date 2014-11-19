453881bc8eee3f1fda37b7179f276ac5f7c60572
Evgeniy <job.egavr@yandex.ru>
2014-11-19 19:20:15 +0300

    Some fixes for Windows OS

--- test/unit/magic-config.test.js
+++ test/unit/magic-config.test.js
@@ -1,169 +1,178 @@
-var MagicConfig = require('./../../lib/magic-config');
+var MagicConfig = require('./../../lib/magic-config'),
+    path = require('path'),
+    nodePath = path.join('path', 'to', 'node'),
+    targetPath = path.join(nodePath, 'target');
 
 describe('magic-config', function () {
     it('must register target', function () {
         var config = new MagicConfig();
 
-        config.registerTarget('path/to/node/target');
-        config.isRegisteredTarget('path/to/node/target').must.be.true();
+        config.registerTarget(targetPath);
+        config.isRegisteredTarget(targetPath).must.be.true();
     });
 
     it('must register existing target', function () {
         var config = new MagicConfig();
 
-        config.registerTarget('path/to/node/target');
-        config.registerTarget('path/to/node/target');
+        config.registerTarget(targetPath);
+        config.registerTarget(targetPath);
 
-        config.getRegisteredTargets().must.eql(['path/to/node/target']);
+        config.getRegisteredTargets().must.eql([targetPath]);
     });
 
     it('must register targets', function () {
         var config = new MagicConfig(),
             nodes;
 
-        config.registerTargets(['path/to/node/target-1', 'path/to/node/target-2']);
+        config.registerTargets([
+            path.join(nodePath, 'target-1'),
+            path.join(nodePath, 'target-2')
+        ]);
         nodes = config.getRegisteredTargets();
 
         nodes.must.have.length(2);
-        nodes.must.include('path/to/node/target-1');
-        nodes.must.include('path/to/node/target-2');
+        nodes.must.include(path.join(nodePath, 'target-1'));
+        nodes.must.include(path.join(nodePath, 'target-2'));
     });
 
     it('must register node', function () {
         var config = new MagicConfig();
 
-        config.registerNode('path/to/node');
-        config.isRegisteredNode('path/to/node').must.be.true();
+        config.registerNode(nodePath);
+        config.isRegisteredNode(nodePath).must.be.true();
     });
 
     it('must register existing node', function () {
         var config = new MagicConfig();
 
-        config.registerNode('path/to/node');
-        config.registerNode('path/to/node');
+        config.registerNode(nodePath);
+        config.registerNode(nodePath);
 
-        config.getRegisteredNodes().must.eql(['path/to/node']);
+        config.getRegisteredNodes().must.eql([nodePath]);
     });
 
     it('must register nodes', function () {
         var config = new MagicConfig(),
             nodes;
 
-        config.registerNodes(['path/to/node-1', 'path/to/node-2']);
+        config.registerNodes([path.join('path', 'to', 'node-1'), path.join('path', 'to', 'node-2')]);
         nodes = config.getRegisteredNodes();
 
         nodes.must.have.length(2);
-        nodes.must.include('path/to/node-1');
-        nodes.must.include('path/to/node-2');
+        nodes.must.include(path.join('path', 'to', 'node-1'));
+        nodes.must.include(path.join('path', 'to', 'node-2'));
     });
 
     it('must detect node by registered target', function () {
         var config = new MagicConfig();
 
-        config.registerTarget('path/to/node/target');
-        config.hasNode('path/to/node').must.be.true();
+        config.registerTarget(path.join(nodePath, 'target'));
+        config.hasNode(nodePath).must.be.true();
     });
 
     it('must detect node by registered node', function () {
         var config = new MagicConfig();
 
-        config.registerNode('path/to/node');
-        config.hasNode('path/to/node').must.be.true();
+        config.registerNode(nodePath);
+        config.hasNode(nodePath).must.be.true();
     });
 
     it('must get nodes if has registered node & target', function () {
         var config = new MagicConfig(),
             nodes;
 
-        config.registerNode('path/to/node-1');
-        config.registerTarget('path/to/node-2/target');
+        config.registerNode(path.join('path', 'to', 'node-1'));
+        config.registerTarget(path.join('path', 'to', 'node-2', 'target'));
         nodes = config.getNodes();
 
         nodes.must.have.length(2);
-        nodes.must.include('path/to/node-1');
-        nodes.must.include('path/to/node-2');
+        nodes.must.include(path.join('path', 'to', 'node-1'));
+        nodes.must.include(path.join('path', 'to', 'node-2'));
     });
 
     it('must register target if it is included in registered node', function () {
         var config = new MagicConfig();
 
-        config.registerNode('path/to/node');
-        config.registerTarget('path/to/node/target');
+        config.registerNode(nodePath);
+        config.registerTarget(targetPath);
 
-        config.isRegisteredTarget('path/to/node/target').must.be.true();
+        config.isRegisteredTarget(targetPath).must.be.true();
     });
 
     it('must not register dot node', function () {
         var config = new MagicConfig();
 
-        config.registerNode('path/to/.node');
+        config.registerNode(path.join('path', 'to', '.node'));
 
-        config.isRegisteredNode('path/to/.node').must.be.false();
+        config.isRegisteredNode(path.join('path', 'to', '.node')).must.be.false();
     });
 
     it('must not register dot target', function () {
         var config = new MagicConfig();
 
-        config.registerTarget('path/to/node/.target');
+        config.registerTarget(path.join(nodePath, '.target'));
 
-        config.isRegisteredNode('path/to/node/.target').must.be.false();
+        config.isRegisteredNode(path.join(nodePath, '.target')).must.be.false();
     });
 
     it('must fit any target if not specified requiredTargets', function () {
         var config = new MagicConfig();
 
-        config.isRequiredTarget('path/to/node/target').must.be.true();
+        config.isRequiredTarget(targetPath).must.be.true();
     });
 
     it('must considered equal path with last slash and path without slash', function () {
-        var config = new MagicConfig(['path/with/last-slash/', 'path/without/last-slash']);
+        var config = new MagicConfig([
+            path.join('path', 'with', 'last-slash', path.sep),
+            path.join('path', 'without', 'last-slash')
+        ]);
 
-        config.isRequiredTarget('path/without/last-slash/').must.be.true();
-        config.isRequiredTarget('path/without/last-slash').must.be.true();
+        config.isRequiredTarget(path.join('path', 'without', 'last-slash', path.sep)).must.be.true();
+        config.isRequiredTarget(path.join('path', 'without', 'last-slash')).must.be.true();
 
-        config.isRequiredTarget('path/with/last-slash/').must.be.true();
-        config.isRequiredTarget('path/with/last-slash').must.be.true();
+        config.isRequiredTarget(path.join('path', 'with', 'last-slash', path.sep)).must.be.true();
+        config.isRequiredTarget(path.join('path', 'without', 'last-slash')).must.be.true();
     });
 
     it('must detect required node', function () {
-        var config = new MagicConfig(['path/to/node']);
+        var config = new MagicConfig([nodePath]);
 
-        config.isRequiredNode('path/to/node').must.be.true();
+        config.isRequiredNode(nodePath).must.be.true();
     });
 
     it('must detect required target', function () {
-        var config = new MagicConfig(['path/to/node/target']);
+        var config = new MagicConfig([targetPath]);
 
-        config.isRequiredTarget('path/to/node/target').must.be.true();
+        config.isRequiredTarget(targetPath).must.be.true();
     });
 
     it('must detect target in required node', function () {
-        var config = new MagicConfig(['path/to/node']);
+        var config = new MagicConfig([nodePath]);
 
-        config.isRequiredTarget('path/to/node/target').must.be.true();
+        config.isRequiredTarget(targetPath).must.be.true();
     });
 
     it('must not detect target in non required node', function () {
-        var config = new MagicConfig(['path/to/other-node']);
+        var config = new MagicConfig([path.join('path', 'to', 'other-node')]);
 
-        config.isRequiredTarget('path/to/node/target').must.be.false();
+        config.isRequiredTarget(targetPath).must.be.false();
     });
 
     it('must detect node in deep required node', function () {
-        var config = new MagicConfig(['path/to/node']);
+        var config = new MagicConfig([nodePath]);
 
-        config.isRequiredNode('path/to/node/deep-node').must.be.true();
+        config.isRequiredNode(path.join(nodePath, 'deep-node')).must.be.true();
     });
 
     it('must detect target in deep required node', function () {
-        var config = new MagicConfig(['path/to/node']);
+        var config = new MagicConfig([nodePath]);
 
-        config.isRequiredTarget('path/to/node/deep-node/target').must.be.true();
+        config.isRequiredTarget(path.join(nodePath, 'deep-node', 'target')).must.be.true();
     });
 
     it('must detect node if required target in specified node', function () {
-        var config = new MagicConfig(['path/to/node/target']);
+        var config = new MagicConfig([targetPath]);
 
-        config.isRequiredNode('path/to/node').must.be.true();
+        config.isRequiredNode(nodePath).must.be.true();
     });
 });
